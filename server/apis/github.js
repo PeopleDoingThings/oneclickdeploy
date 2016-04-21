bda08ba57f59e778d79462cc89f4ce92caf462c8
Patrick Lynch <patrickflynch@gmail.com>
2016-04-21 12:34:08 -0500

    In the middle of github helpers

--- server/apis/github.js
+++ server/apis/github.js
@@ -1,1 +1,25 @@
-var req = require('request');
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
+Promise.promisifyAll(request);
+
+exports.getUserRepos = function(user) {
+
+  var options = {
+    url: 'https://api.github.com/users/' + user + '/repos',
+    headers: {
+      'User-Agent': 'peopleDoingThings/oneclickdeploy'
+    }
+  };
+
+  var url = "https://api.github.com/users/" + user + "/repos"
+
+  return request(options)
+    .then(function(data){
+      console.log("/server/api/routes/github.js line 11 - GOT REPO DATA")
+      return data;
+    })
+    .catch(function(err) {
+      console.log("/server/api/routes/github.js line 15 - ERROR: ", err)
+      return err;
+    })
+}
--- server/apis/routes/githubroutes.js
+++ server/apis/routes/githubroutes.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var GITHUB = require('../github.js');
 
 // These routes are relative to the mounted router. Therefore '/' here is actaully '/api/github'. 
 
-router.get('/', function(req, res) {
+router.get('/repos', function(req, res) {
+
+  var testUser = "chppr"
+
+  GITHUB.getUserRepos(testUser)
+  .then(function(resp) {
+
+    var data = JSON.parse(resp.body)
+
+    var filtered = data.map(function(repo) {
+      var condensed = {};
+
+        condensed.id = repo.id;
+        condensed.name = repo.name;
+        condensed.clone_url = repo.clone_url;
+        condensed.contents_url = repo.contents_url;
+        condensed.procfile_url = condensed.contents_url.replace("{+path}", "Procfile")
+
+      return condensed;
+    });
+
+    res.send(filtered);
+  })
+  .catch(function(err) {
+    console.log('Error finding github repos for user:', err);
+    res.send(err);
+  })
+
 
 })
-
-
-
-
-
 
 module.exports = router;
