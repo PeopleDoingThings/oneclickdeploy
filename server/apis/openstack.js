c4f58358c1b33c836da4ef5dfb0539832721ca5b
Patrick Lynch <patrickflynch@gmail.com>
2016-04-20 19:49:42 -0500

    Update openstack.js
    formatting

--- server/apis/openstack.js
+++ server/apis/openstack.js
@@ -6,13 +6,13 @@ var imageEndpoint = 'https://image.compute.bhs1.cloud.ovh.net/';
 var identityEndpoint = 'https://auth.cloud.ovh.net/v2.0';
 
 var createOpts = function(url) {
-	var opt = {
-  	url: url',
-  	headers: {
-    	'X-Auth-Token': process.env.OPENSTACK_X_AUTH
-  	}
-	};
+  var opt = {
+    url: url,
+    headers: {
+      'X-Auth-Token': process.env.OPENSTACK_X_AUTH
+    }
+  };
 
-	return opt;
+  return opt;
 }
 
