241f23d06687a499eed97f7ebdc46e6765bb258c
Harry Hough <bluestreek@gmail.com>
2016-04-21 15:40:29 -0500

    Restructured. (#20)
    * Adding additional routes & appropriate query params. OVH APIs
    
    * Added logic folder to clean up ovh apis logic. Also added '/listservices' endpoint returning an array of all active instances & some info.
    
    * Automated OpenStack Re-auths & added flavor methods.
    
    * Fixed tabs to spaces.
    
    * Preparing endpoints neccesary to create a new instance!
    
    * Added more calls neccessary for OVH create instance API. Also wrote the worlds most beautiful Arrow Functions.
    
    * Restructured for better organization & separation of concerns.
    
    * Refactored OVH helpers.js to arrow functions.
    
    * Refactored OpenStack API calls with helper.js in logic folder. This better matches overall code module pattern of project.

--- server/apis/routes/ovhroutes.js
+++ server/apis/routes/ovhroutes.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var OVH = require('../ovh.js');
-var Logic = require('../logic/logic.js');
+var Logic = require('../logic/ovh/logic.js');
 
 // These routes are relative to the mounted router. Therefore '/' here is actually '/api/ovh'. 
-
-//example ids.
-var projectid = '614a903725ef470e8784b48758da23ad';
-var insid = '5dd13a9d-9d45-48d8-9a9a-04ab739d7623';
-
 
 // Time ex: 'today' 'lastday' 'lastweek'
 // Can request 'mem:used' 'cpu:used' 'net:tx' 'net:rx'
@@ -18,10 +13,10 @@ router.get('/usagestatistics', function(req, res) {
 		res.send(resp);
 	})
 	.catch(function(err) {
-		console.log('ovhroutes 21 err = ', err);
 		res.send(err);
 	})
 })
+
 
 // Retrieve a list of all active instances & information for each in an Array.
 router.get('/listservices', function(req, res) {
@@ -29,9 +24,19 @@ router.get('/listservices', function(req, res) {
 		res.send(data);
 	})
 	.catch(function(err) {
-		console.log('ovhroutes 31: err = ', err);
 		res.send(err);
 	})
+})
+
+
+// Returns the flavor id & the image id
+router.get('/vpsimageflavor/:version', function(req, res) {
+  Logic.imageData(req.params.version).then(function(data) {
+    res.send(data);
+  })
+  .catch(function(err) {
+    res.send(err.message);
+  })
 })
 
 
