1944bd03dbe2e9c062fa0ab8ee4bd6652ceece49
bluestreek18 <bluestreek@gmail.com>
2016-04-20 21:55:28 -0500

    Adding additional routes & appropriate query params. OVH APIs

--- server/apis/routes/ovhroutes.js
+++ server/apis/routes/ovhroutes.js
@@ -4,22 +4,32 @@ var OVH = require('../ovh.js');
 
 // These routes are relative to the mounted router. Therefore '/' here is actually '/api/ovh'. 
 
+//example ids.
 var projectid = '614a903725ef470e8784b48758da23ad';
-var iid = '5dd13a9d-9d45-48d8-9a9a-04ab739d7623';
+var insid = '5dd13a9d-9d45-48d8-9a9a-04ab739d7623';
 
-router.get('/data', function(req, res) {
-	OVH.getMemUsage(iid, projectid).then(function(resp) {
+
+// Time ex: 'today' 'lastday' 'lastweek'
+// Can request 'mem:used' 'cpu:used' 'net:tx' 'net:rx'
+router.get('/usagestatistics', function(req, res) {
+	// Likely want to add req.query insid & proj id etc.
+	OVH.getInstanceUsage(insid, projectid, req.query.time, req.query.type).then(function(resp) {
 		res.send(resp);
 	})
 	.catch(function(err) {
-		console.log('errr gettig mee usage = ', err)
 		res.send(err);
 	})
 })
 
-
-
-
+router.get('/listservices', function(req, res) {
+	// List avaliable instances/cloud projects
+	OVH.listServices().then(function(resp) {
+		res.send(resp);
+	})
+	.catch(function(err) {
+		res.send(err);
+	})
+})
 
 
 module.exports = router;
