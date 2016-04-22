3ac3fff469ac79afe5d0f0978e052186c3b4891d
Harry Hough <bluestreek@gmail.com>
2016-04-22 11:31:01 -0500

    Removed an image id.

--- server/apis/logic/ovh/helpers.js
+++ server/apis/logic/ovh/helpers.js
@@ -17,7 +17,6 @@ exports.checkFlavorData = function(data) {
 }
 
 // Need this string in userdata as its a cloudinit config allow us to set an ssh password.
-// 0bae6cd8-b946-44da-b79d-0d6f65ea4f42   //ID of our snapshot/custom img!
 exports.createInstanceObj = function(flavor, img, name, password) {
   var obj = {
     flavorId: flavor,
