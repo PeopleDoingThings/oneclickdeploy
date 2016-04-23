e38f712ffd5ab5513604ab2d0c2ca8abee1186a6
bluestreek18 <bluestreek@gmail.com>
2016-04-23 14:35:21 -0500

    Added Instances Schema & Methods. Also restructured slightly to make sure all models are in the database folder.

---
+++ server/database/models/instance.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+// State is the state of deployment our instance is in. This lets is resume from a previous state on errors.
+// The repos array contains objects which list the github repo url etc.
+// The ownergitid is the github account who is associated with the deployment.
+// We can update 'repos' using:
+// db.update({'Searching criteria goes here'}, 
+//   { $push : {
+//     repos: {
+//       "url": url,
+//       "domain": 'mydomain.com'
+//     } //inserted data is the object to be inserted 
+//   }
+// });
+
+
+var Instance = new Schema({
+  name: String,
+  openstackid: String,
+  ownergitid: String,
+  state: {
+    built: Boolean,
+    customimage: Boolean,
+    postinstall: Boolean,
+    repos: { type : Array, "default" : [] }
+  },
+  creationdate: String,
+  publicip: String,
+  image: {
+    flavorid: String,
+    imageid: String,
+    osname: String,
+    defaultuser: String
+  },
+  region: String
+});
+
+
+module.exports = mongoose.model('Instance', Instance);
