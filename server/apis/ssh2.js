eaf0e1a89ec5c507e0dfcb8969bd27a88f8e6534
bluestreek18 <bluestreek@gmail.com>
2016-04-23 19:29:51 -0500

    SSH2 Routes Enabled

--- server/apis/ssh2.js
+++ server/apis/ssh2.js
@@ -1,10 +1,21 @@
 var req = require('request');
 var Helpers = require('./logic/ssh2/helpers.js');
+var Instance = require('../database/instances.js')
 //This methods file includes our logic and isolates it from our API.
 var Methods = require('../ssh2/methods.js');
 var Commands = require('../ssh2/dblists/commands.js');
 
 
-exports.runSSHPostInstallSetup = function(instanceid) {
-  
+exports.runSSHPostInstallSetup = function(instanceid, cmdArray) {
+  return Instance.getInstaceById(instanceid)
+    .then(function(data) {
+      return Methods.runCommandList(data, cmdArray);
+    })
+    .then(function(data) {
+      return data;
+    })
+    .catch(function(err) {
+      return err;
+    })
+
 }
