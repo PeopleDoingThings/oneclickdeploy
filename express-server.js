c7b9994fa015cacfcb07dabc1d36106522b02ee5
bluestreek18 <bluestreek@gmail.com>
2016-04-24 18:00:34 -0500

    Added back deleted comments.

--- express-server.js
+++ express-server.js
@@ -44,3 +44,71 @@ app.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
+
+// app.use('*', function(req, res) {
+//   res.redirect("/");
+// });
+
+// //rubix magic trick
+// app.get('*', function(req, res, next) {
+//   renderHTML(req, res, next);
+// });
+
+// var fs = require('fs');
+// var path = require('path');
+
+// global.window = global;
+// global.navigator = {
+//   userAgent: {
+//     indexOf: function() {return true;},
+//     toLowerCase: function() {
+//       return "";
+//     }
+//   }
+// };
+
+// //window.$ = require('jquery');
+
+// global.React = require('react');
+// global.ReactRouter = require('react-router');
+
+// var ReactDOMServer = require('react-dom/server');
+// var routes = require('./dist/bundle.js');
+
+
+// global.renderHTML = function(req, res, next, store) {
+//   if(req.url === '/favicon.ico'
+//     || (req.url.search('.l20n') !== -1)) return next();
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+//   var isRTL = req.cookies ? true : false;
+
+//   var data = "";
+//   if (store) {
+//     data = escape(encodeURIComponent(JSON.stringify(store.getState())));
+//   }
+
+//   ReactRouter.match({
+//     routes: routes({ listen: () => {} }),
+//     location: req.url
+//   }, function(err, redirectLocation, renderProps) {
+//     if(err) {
+//       res.status(500).send(err.message);
+//     } else if(redirectLocation) {
+//       res.redirect(redirectLocation.pathname + redirectLocation.search);
+//     } else if(renderProps) {
+//       var str = renderDOMString(store, data, renderProps);
+//       if(isRTL) {
+//         str = rtl.replace(new RegExp('{container}', 'g'), str);
+//         str = str.replace(new RegExp('{server_data}', 'g'), data);
+//         res.status(200).send(str);
+//       } else {
+//         str = ltr.replace(new RegExp('{container}', 'g'), str);
+//         str = str.replace(new RegExp('{server_data}', 'g'), data);
+//         res.status(200).send(str);
+//       }
+//     } else {
+//       res.status(404).send('Not found');
+//     }
+//   });
+// };
