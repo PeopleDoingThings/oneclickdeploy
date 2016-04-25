95d92e418d0e536464e986dfc17e76955588cc69
Patrick Lynch <patrickflynch@gmail.com>
2016-04-25 12:41:05 -0500

    Added middleware to instance daemon (#56)

--- instance-monitor/server/daemon.js
+++ instance-monitor/server/daemon.js
@@ -1,9 +1,31 @@
 var express = require('express')
 var Path = require('path')
+var tokenList = require('../token_list.js')
 
 var app = express()
 
 const exec = require('child_process').exec;
+
+app.use(function(req, res, next) {
+  var parentToken = req.headers['parent-server-token']
+  var validity = tokenList.indexOf(parentToken) !== -1
+
+    console.log("--------------------------------------------")
+    console.log("Request:")
+    console.log("--------------------------------------------")
+    console.log("tokenList: ", tokenList)
+    console.log("--------------------------------------------")
+    console.log("parentToken: ", parentToken)
+    console.log("--------------------------------------------")
+    console.log("validity: ", validity)
+    console.log("--------------------------------------------")
+
+  if (tokenList.indexOf(parentToken) === -1) {
+    res.sendStatus(403)
+  } else {
+    next()
+  }
+})
 
 app.get('/statistics/top', function(req, res){
   // set this environment on each instance
@@ -31,45 +53,27 @@ app.get('/statistics/top', function(req, res){
   })
 })
 
-app.get('/statistics/forever', function(req, res){
+app.use(function(req, res, next) {
+  var parentToken = req.headers['parent-server-token']
+  var validity = tokenList.indexOf(parentToken) !== -1
 
-  command = 'forever list'
+    console.log("--------------------------------------------")
+    console.log("Request:")
+    console.log("--------------------------------------------")
+    console.log("tokenList: ", tokenList)
+    console.log("--------------------------------------------")
+    console.log("parentToken: ", parentToken)
+    console.log("--------------------------------------------")
+    console.log("validity: ", validity)
+    console.log("--------------------------------------------")
 
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      console.error('error running forever list', err);
-      res.send(err);
-    } else {
-      var response = stdout.split('\n').filter(line => line.length > 0)
-      console.log("---------------------------------------------------------")
-      console.log("- recieved request - /statistics/forever ; response:    -")
-      console.log("---------------------------------------------------------")
-      console.log(response);
-      console.log("---------------------------------------------------------")
-      res.json(response);
-    }
-  })
+  if (tokenList.indexOf(parentToken) === -1) {
+    res.sendStatus(403)
+  } else {
+    next()
+  }
 })
 
-app.get('/statistics/uptime', function(req, res){
-
-  command = 'uptime'
-
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      console.error('error running uptime', err);
-      res.send(err);
-    } else {
-      var response = stdout.split('\n').filter(line => line.length > 0)
-      console.log("---------------------------------------------------------")
-      console.log("- recieved request - /statistics/uptime ; response:    -")
-      console.log("---------------------------------------------------------")
-      console.log(response);
-      console.log("---------------------------------------------------------")
-      res.json(response);
-    }
-  })
-})
 
 // Parse incoming request bodies as JSON
 app.use( require('body-parser').json() )
