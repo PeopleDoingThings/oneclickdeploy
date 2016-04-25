ffe7c86c658a4984a94cb4bd575ca7296f06848f
Patrick Lynch <patrickflynch@gmail.com>
2016-04-25 12:33:55 -0500

    Added middleware to instance daemon

---
+++ instance-monitor/token_list.js
@@ -0,0 +1,7 @@
+module.exports = [
+      //test token
+      'notarealtoken'
+//  , 'put other tokens here'
+//  , 'leading commas; yay or nay?'
+//  , 'the above would make a great header token'
+]
