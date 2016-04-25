733b0c7977b3e443393e07a4ab62278d953fef28
Patrick Lynch <patrickflynch@gmail.com>
2016-04-25 11:12:53 -0500

    add forever list and uptime routes to instance app (#52)

--- instance-monitor/server/daemon.js
+++ instance-monitor/server/daemon.js
@@ -16,18 +16,58 @@ app.get('/statistics/top', function(req, res){
   }
 
   exec(topstring, (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-    res.send(err);
-  } else {
-    var response = stdout.split('\n').filter(line => line.length > 0)
-    console.log("---------------------------------------------------------")
-    console.log("- recieved request for /statistics/top ; response:      -")
-    console.log("---------------------------------------------------------")
-    console.log(response);
-    console.log("---------------------------------------------------------")
-    res.json(response);
-  }
+    if (err) {
+      console.error(err);
+      res.send(err);
+    } else {
+      var response = stdout.split('\n').filter(line => line.length > 0)
+      console.log("---------------------------------------------------------")
+      console.log("- recieved request for /statistics/top ; response:      -")
+      console.log("---------------------------------------------------------")
+      console.log(response);
+      console.log("---------------------------------------------------------")
+      res.json(response);
+    }
+  })
+})
+
+app.get('/statistics/forever', function(req, res){
+
+  command = 'forever list'
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error('error running forever list', err);
+      res.send(err);
+    } else {
+      var response = stdout.split('\n').filter(line => line.length > 0)
+      console.log("---------------------------------------------------------")
+      console.log("- recieved request - /statistics/forever ; response:    -")
+      console.log("---------------------------------------------------------")
+      console.log(response);
+      console.log("---------------------------------------------------------")
+      res.json(response);
+    }
+  })
+})
+
+app.get('/statistics/uptime', function(req, res){
+
+  command = 'uptime'
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error('error running uptime', err);
+      res.send(err);
+    } else {
+      var response = stdout.split('\n').filter(line => line.length > 0)
+      console.log("---------------------------------------------------------")
+      console.log("- recieved request - /statistics/uptime ; response:    -")
+      console.log("---------------------------------------------------------")
+      console.log(response);
+      console.log("---------------------------------------------------------")
+      res.json(response);
+    }
   })
 })
 
