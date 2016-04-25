2f923a1a90470473702b977fb830fea0e6fc6b75
Harry Hough <bluestreek@gmail.com>
2016-04-25 10:02:33 -0500

    Added Route to get user Instances! (#50)
    * Added Route to get user Instances!
    
    * Fixed bug in get user instance list.

---
+++ server/apis/routes/dbroutes.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var Instance = require('../../database/instances');
+
+
+// Protect with middleware later on.
+router.get('/getuserinstances', function(req, res) {
+  return Instance.getUserInstances(req.user.gitid)
+    .then(function(data) {
+      res.send(data);
+    })
+    .catch(function(err) {
+      res.send(err.message);
+    })
+
+})
+
+
+
+module.exports = router;
