780b1706d534cabd41f6cf6718726f42761d08b6
febtek <f.teklemicael@gmail.com>
2016-04-24 22:06:40 -0500

    Feature/user profile2 (#48)
    * Show user profile
    
    * complete logout (url needs to be replaced)
    
    * complete deploy fct (endpoint needs to be replaced once ready)

--- client/containers/sideBar.js
+++ client/containers/sideBar.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import User from '../components/user';
+import Logout from '../components/logout';
 
 export default class Aside extends Component {
+ constructor(props){
+    super(props);
+    console.log('props in aside:' ,this.props);
+  
+  }
+
   render() {
     return (
       <aside>
+      <User user={this.props.user}/>
         <section className="pull-left">
           <nav>
             <ul className="nav nav-pills">
+              Go To:
               <li className="icon-bar"><a href='/#/repos'>Repos List</a></li>
-              <li classNave="icon-bar"><a href='/#/dashboard'>dashboard</a></li>
+              <li classNave="icon-bar"><a href='/#/dashboard'>Dashboard</a></li>
+              <Logout />
             </ul>
           </nav>
           </section>
