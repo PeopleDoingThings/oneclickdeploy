13ea47d56f7d5ef5ff112f36a0a8f94887427b10
bluestreek18 <bluestreek@gmail.com>
2016-04-25 22:47:17 -0500

    Removed a hardcoded gitid from testing.

--- server/apis/routes/githubroutes.js
+++ server/apis/routes/githubroutes.js
@@ -6,13 +6,14 @@ var Logic = require('../logic/github/logic.js');
 // These routes are relative to the mounted router. Therefore '/' here is actaully '/api/github'.
 
 router.get('/repos', function(req, res) {
+  // Must now be logged in & have a repo with Procfile for this route!
   console.log('Logged In GitHub User: ', req.user && req.user.login)
-  var username = req.user && req.user.login || 'plynch';
+  var username = req.user && req.user.login;
 
   Github.getUserRepos(username)
     .then(function(resp) {
       console.log('got repos! = ', resp)
-      Logic.save('512480', resp)  // replace string here with req.user.gitid
+      Logic.save(req.user.gitid, resp)  // replace string here with req.user.gitid
         .then(function(data) {
           console.log('==== githubroutes data ==== ', data);
         })
