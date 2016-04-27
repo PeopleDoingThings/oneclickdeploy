d3c467b83cde91e1c09bb92e2546c6f8d47d925a
Feben Teklemicael <f.teklemicael@gmail.com>
2016-04-27 01:21:20 -0500

    set up 3rd interval for logout put -mostly working, getlogoutput returs unauth

--- client/containers/loadingInstance.js
+++ client/containers/loadingInstance.js
@@ -1,46 +1,65 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux'; 
 import { connect } from 'react-redux';
+import { Link, browserHistory } from 'react-router';
 import { instanceReady, getLog, isDeployed, sshPostInstall } from '../actions/index';
 
 class Loading extends Component {
   constructor(props){
-    super(props); 
-
+  super(props); 
+  this.props.getLog();
   var component = this ;  
   component.props.instanceReady()
-  //set interval for check instance
-  let cksInsID1 ='';
-  function startChckInstInterval() {     
-      cksInsID1 = setInterval(function () {
-        console.log(' set timeout props checkInst', component.props.InstStatus)    
-          if(component.props.InstStatus=== undefined){
-            console.log('inst status undefiend ')
-          }
-          if(component.props.InstStatus === true){        
-            console.log('if statement InstStatus is true')
-            console.log('about to stop Interval')
-           //call postinstall endpoint
-            component.props.sshPostInstall();
-            console.log('called postinstall')
-           //start set interval2/check deployed 
-            startChckDeployedInterval();
-           //stop set interval1 if it already started 
-            stopChckInstInterval(); 
-          }
-          else if (component.props.InstStatus === false) {
-            //if still false call api again
-            console.log('check instance ready is still false')
-           component.props.instanceReady();
-          }
-      }, 1000);
-  }
+//set interval for logoutput
+let logOutput0 ='';
+function startLogOutputInterval() {     
+    logOuput0 = setInterval(function () {
+        component.props.getLog();
+        console.log('what is logoutput in interval',component.props.LogOutput)
+      }, 50);
+}
 
-  function stopChckInstInterval() {
+function stopLogOutputInterval() {
+  clearInterval(logOuput0);
+  console.log('really stopped logoutput now')
+}
+
+startLogOutputInterval();
+
+//set interval for check instance
+let cksInsID1 ='';
+function startChckInstInterval() {     
+    cksInsID1 = setInterval(function () {
+      console.log(' set timeout props checkInst', component.props.InstStatus)    
+        if(component.props.InstStatus=== undefined){
+          console.log('inst status undefiend ')
+        }
+        if(component.props.InstStatus === true){        
+          console.log('if statement InstStatus is true')
+          console.log('about to stop Interval')
+         //call postinstall endpoint
+          component.props.sshPostInstall();
+          console.log('called postinstall')
+         //start set interval2/check deployed 
+          startChckDeployedInterval();
+         //stop set interval1 if it already started 
+          stopChckInstInterval(); 
+        }
+        else if (component.props.InstStatus === false) {
+          //if still false call api again
+          console.log('check instance ready is still false')
+          component.props.instanceReady();
+        }
+    }, 5000);
+}
+
+function stopChckInstInterval() {
   clearInterval(cksInsID1);
-  console.log('really stopped now')
+  console.log('really stopped  checkinst ready interval now')
 } 
+
 startChckInstInterval();
+
 //set interval for checkdeployed
 let ckDep2 ='';
 function startChckDeployedInterval() {     
@@ -52,11 +71,10 @@ function startChckDeployedInterval() {
         if(component.props.DeployedStatus === true){           
           console.log('isDeployed statement is true')
           console.log('about to stop Interval 2 checkdeployed')
-
          //stop interval2 log output
-                  //stopLogOutputInterval();
-                  //transition to DB
-                  //code for transiotn to DB
+        stopLogOutputInterval();
+        //transition to DB
+         window.location = 'http://localhost:9001/#/dashboard';
          stopChckDeployedInterval(); 
          //redirect to dashboard
         }
@@ -65,60 +83,40 @@ function startChckDeployedInterval() {
           console.log('check isDeployed ready is still false')
           component.props.isDeployed();
         }
-    }, 4000);
+    }, 10000);
   }
 
-  function stopChckDeployedInterval() {
+function stopChckDeployedInterval() {
   clearInterval(ckDep2);
-  console.log('really stopped now')
+  console.log('really stopped chkdeployed interval now')
 }
-
 
 }
  
   render() {
-    //console.log('props in render:', this.props)//log state.status later
+    console.log('props in render:', this.props)//log state.status later
     return (
       <div>
         <h1>Loading.....</h1>
         <div>Testing Log</div>
-         {
-          //console.log('in render logging this.props', this.props.InstStatus)
-         }
-        <div>Instance info (repo url for testing):</div><div> {this.props.InstData[0] ? this.props.InstData[0].id : null} )</div>      
+        <div>Instance info:</div><div> {this.props.InstData[0] ? this.props.InstData[0].id : null} )</div>   
+        <div>Log Output</div>{this.props.logOutput}   
       </div>
     );
   }
 }
 
 
-// // // const setIntervalLog = function (getLogfct,params) {
-// // //    setInterval(() => {
-// // //       getLogfct(params);
-// // //     }, 1000);
-// // // }
-
-// // //set interval for load data
-// //           // let logOuput0 ='';
-// //           // function  startLogOutputInterval() {
-// //           //     logOuput0 = setInterval(() => {
-// //           //         console.log('working ckInstnace');
-// //           //     }, 1000);
-// //           //   }
-
-// //           // function stopLogOutputInterval() {
-// //           //   clearInterval(logOuput0);
-// //           // }
-
 function mapStateToProps(state) {
-  console.log('load state: ', state.reducers.load)
-  console.log('install data in loading state', state.reducers.install)
-  console.log('state of deploy status reducers', state.reducers.isDeployed[0])
+  // console.log('load state: ', state.reducers.load)
+  // console.log('install data in loading state', state.reducers.install)
+  console.log('state of log reducers', state.reducers.logOutput)
   return {
     Load: state.reducers.load,
     InstData: state.reducers.install,
     InstStatus: state.reducers.instReady.isReady,
-    DeployedStatus: state.reducers.isDeployed[0] ? state.reducers.isDeployed[0].deployed : null
+    DeployedStatus: state.reducers.isDeployed[0] ? state.reducers.isDeployed[0].deployed : null,
+    LogOutput: state.reducers.logOutput
   };
 }
 
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -5,13 +5,15 @@ import IsAuth from './reducer_auth';
 import InstReady from './reducer_instReady';
 import CreatInstance from './reducer_creatInst';
 import IsDeployed from './reducer_isDeployed';
+import LogOutput from './reducer_logOutput';
 
 const rootReducer = combineReducers({
   repos: ReposReducer,
   auth: IsAuth,
   install: CreatInstance,
   instReady: InstReady,
-  isDeployed: IsDeployed
+  isDeployed: IsDeployed,
+  logOutput: LogOutput
 });
 
 
