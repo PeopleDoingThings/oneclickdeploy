6e86fdefaad314bd4d9df71d9acf0ea14f496373
Patrick Lynch <patrickflynch@gmail.com>
2016-04-27 15:34:27 -0500

    better token logic (#67)

--- instance-monitor/token_list.js
+++ instance-monitor/token_list.js
@@ -1,7 +1,3 @@
 module.exports = [
-      //test token
-      'notarealtoken'
-//  , 'put other tokens here'
-//  , 'leading commas; yay or nay?'
-//  , 'the above would make a great header token'
-]
+  // 'add_token_here'
+];
