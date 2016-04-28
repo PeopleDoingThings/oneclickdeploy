6caf71c63df039fa2358926c8c75b088f3ce3a7f
bluestreek18 <bluestreek@gmail.com>
2016-04-28 14:57:10 -0500

    More refactoring.

--- server/login/gitlogin.js
+++ server/login/gitlogin.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var InstanceDB = require('../database/instances.js');
-
-
 var passportGithub = require('./auth/githubstrategy.js');
 
 // These routes are relative to the mounted router. Therefore '/' here is actaully '/login/'. 
@@ -10,15 +8,9 @@ var passportGithub = require('./auth/githubstrategy.js');
 
 //req.isAuthenticated() will return true or false. Then returns the user obj if they are authed.
 router.get('/isauthenticated', function(req, res) {
-  console.log('req.user === ', req.user)
-
-  if (req.isAuthenticated()) {
-    res.send(req.user);
-  } 
-  else {
-    res.sendStatus(401);
-  }
-
+  console.log('Current User: ', req.user.name)
+  if (req.isAuthenticated()) res.send(req.user);
+  else res.sendStatus(401);
 });
 
 router.get('/getuserinstances', function(req, res) {
@@ -30,9 +22,7 @@ router.get('/getuserinstances', function(req, res) {
 router.get('/github', passportGithub.authenticate('github'));
 
 router.get('/github/callback',
-
   passportGithub.authenticate('github', { failureRedirect: 'http://localhost:9001/failed', successRedirect: '/#/main-panel' }));
-
 
 module.exports = router;
 
