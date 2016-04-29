f06e57f7789c0069e09a83ba3f315ef4bc6d295b
bluestreek18 <bluestreek@gmail.com>
2016-04-29 11:35:44 -0500

    Allowed get token route through auth for the moment.

--- server/login/authuser.js
+++ server/login/authuser.js
@@ -1,5 +1,11 @@
 module.exports = function(req, res, next) {
-  if (req.isAuthenticated()) {
+  // TESTING ONLY
+  var requestedUrl = req.protocol + '://' + req.get('Host') + req.url;
+  var final = requestedUrl.split('/');
+  if(final[final.length - 1] === 'getnewtoken') {
+    next();
+  }
+  else if (req.isAuthenticated()) {
     req.user.gitid = String(req.user.gitid);
     console.log('AuthUser Valid!')
     next();
