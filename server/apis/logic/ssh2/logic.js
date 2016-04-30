6c519f69bdfc146f8bddd73f20b9f111fc0fc245
bluestreek18 <bluestreek@gmail.com>
2016-04-29 22:10:10 -0500

    Added reconnect attempts for SSH. Now also returns success or error to endpoint properly.

--- server/apis/logic/ssh2/logic.js
+++ server/apis/logic/ssh2/logic.js
@@ -5,11 +5,52 @@ var Helpers = require('./helpers.js');
 
 
 exports.runSSHPostInstall = function(instanceData, cmdArray, data, repoData) {
-  var host = Helpers.postInstallHost(instanceData, cmdArray, data, repoData);
-  host.debug = true;
+  return new Promise(function(resolve, reject) {
+    var host = Helpers.postInstallHost(instanceData, cmdArray, data, repoData);
+    host.debug = true;
+    var SSHClient = new SSH2Shell(host);
+    var retries = 0;
 
-  var SSHClient = new SSH2Shell(host);
-  return SSHClient.connect();
+        SSHClient.on("error", function onError(err, type, close, callback) {
+          var authFailed = 'All configured authentication methods failed';
+
+          if(err.message === authFailed && retries <= 3) {
+            console.log('Retrying SSH Connection!');
+            console.log('Retry Attempt: ', retries);
+            ++retries;
+          }
+          else {
+            // Send some error to the db.
+            console.log('All Retries Failed, Deploy Fail!');
+            reject( new Error('All configured authentication methods failed!') )
+          }
+
+        console.log('err = ', err.message)
+        console.log('type = ', type)
+        console.log('close = ', close)
+        console.log('retries = ', retries)
+      });
+
+      SSHClient.on("ready", function onReady() { // On successful connect we return a promise.
+        console.log('Connection Ready, Starting Install!')
+        resolve('SSH Connection Successful, Beggining Install');
+      });
+
+      SSHClient.on("close", function onClose(had_error) {
+        console.log('Closing Connection! ', retries)
+
+        if(retries < 3) {
+          SSHClient.connect();
+        }
+        else { // This is to make sure we alway have some way to end the loop. Unlikely to be needed.
+          reject( new Error('All configured authentication methods failed!') )
+        }
+      });
+
+    if(retries === 0) {       // It will start opening exponentially more connections if we don't check here.
+      SSHClient.connect();
+    }
+  })
 }
 
 exports.setDeployed = function(repoData) {
