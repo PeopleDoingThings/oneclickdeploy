d77046f9c7d3a0fa7239406671846db4460d7ea9
bluestreek18 <bluestreek@gmail.com>
2016-05-03 11:07:57 -0500

    Fixed InstanceLoginDB not returning promises properly.

--- server/database/instancelogin.js
+++ server/database/instancelogin.js
@@ -1,14 +1,14 @@
 var InstanceLogin = require('./models/instancelogin.js');
 
 exports.findAndUpdateIP = function(ownerid, ip) {
-  InstanceLogin.find({ ownergitid: ownerid })
+  return InstanceLogin.find({ ownergitid: ownerid })
     .then(function(data) {
       return InstanceLogin.findByIdAndUpdate(data[0]._id, { ip: ip });
     })
 }
 
 exports.findInstanceLogin = function(gitid) {
-  InstanceLogin.find({ ownergitid: gitid })
+  return InstanceLogin.find({ ownergitid: gitid })
     .then(function(data) {
       if(data.length === 0) {
         return Promise.reject( new Error('No Instance Login Credentials Found for User!') )
