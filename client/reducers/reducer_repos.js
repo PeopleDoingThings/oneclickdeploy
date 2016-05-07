50515caa1cddd6fc4b35573d69e43807a9a4cca1
Feben Teklemicael <f.teklemicael@gmail.com>
2016-05-07 10:59:41 -0500

    added refresh button

--- client/reducers/reducer_repos.js
+++ client/reducers/reducer_repos.js
@@ -1,12 +1,14 @@
 //return repos
 //state is state this reducer is responsible for
-import { FETCH_REPOS } from '../actions/index';
+import { FETCH_REPOS, REFRESH_REPO } from '../actions/index';
 
 export default function(state = [], action) {
   switch (action.type) {
 
     case FETCH_REPOS: 
         return action.payload.data === undefined ? state : action.payload.data;
+    case REFRESH_REPO:
+        return action.payload.data === undefined ? state : action.payload.data;	
     default: return state;
 
   }
