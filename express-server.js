10ca27649cf0eeb89cee8d0e7eff85bbc4b217e3
Harry Hough <bluestreek@gmail.com>
2016-05-09 18:55:17 -0500

    Socket.io! (#157)
    * JS servers restart automatically on instance reboot.
    
    * Added socket.io & integrated passport auth with socket.io
    
    * Update index.js
    
    Explicit connect not needed. Perhaps in production for security this is important?

--- express-server.js
+++ express-server.js
@@ -1,19 +1,26 @@
-var express = require('express');
-var app = express();
-var passport = require('passport');
-var session = require('express-session');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var Hat = require('hat');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const ss = require('socket.io-stream');
+const passportSocketIo = require('passport.socketio');
+
+const passport = require('passport');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Hat = require('hat');
+const cookieParser = require('cookie-parser');
 
 //Import Routes
-var LoginRoutes = require('./server/login/gitlogin.js');
-var OVHRoutes = require('./server/apis/routes/ovhroutes.js');
-var OpenStackRoutes = require('./server/apis/routes/openstackroutes.js');
-var GitHubRoutes = require('./server/apis/routes/githubroutes.js');
-var DatabaseRoutes = require('./server/apis/routes/dbroutes.js');
-var SSH2Routes = require('./server/apis/routes/ssh2routes.js');
-var DaemonRoutes = require('./server/apis/routes/daemonroutes.js');
+const LoginRoutes = require('./server/login/gitlogin.js');
+const OVHRoutes = require('./server/apis/routes/ovhroutes.js');
+const OpenStackRoutes = require('./server/apis/routes/openstackroutes.js');
+const GitHubRoutes = require('./server/apis/routes/githubroutes.js');
+const DatabaseRoutes = require('./server/apis/routes/dbroutes.js');
+const SSH2Routes = require('./server/apis/routes/ssh2routes.js');
+const DaemonRoutes = require('./server/apis/routes/daemonroutes.js');
+const sessionSecret = Hat();
 
 // Makes sure express trusts the fowarded ips from Nginx & cloudflare while disabling the ExpressJS header.
 app.enable('trust proxy');
@@ -23,25 +30,25 @@ app.disable('x-powered-by');
 mongoose.connect('mongodb://localhost/oneclickdb');
 
 // Session Store
-var MongoDBStore = require('connect-mongodb-session')(session);
-var store = new MongoDBStore({
+const MongoDBStore = require('connect-mongodb-session')(session);
+const store = new MongoDBStore({
   uri: 'mongodb://localhost:27017/oneclickdb',
   collection: 'sessions'
 });
 
 store.on('error', function(error) {
-  console.log('mstore error!', error);
+  console.log('Mongo Store error!', error);
 });
 
 // Auth Middleware - Make sure user is logged in before continuing.
-var AuthUser = require('./server/login/authuser.js');
+const AuthUser = require('./server/login/authuser.js');
 
 // Add sessions for passport to serialize & let us use get params with urlencoded.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
   name: 'ghv',
-  secret: Hat(),
+  secret: sessionSecret,
   store: store,
   resave: false,
   proxy: true,
@@ -49,7 +56,43 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.listen(process.env.PORT, function() {
+io.use(passportSocketIo.authorize({
+  cookieParser: cookieParser,        // the same middleware you registrer in express
+  key:          'ghv',               // the name of the cookie where express/connect stores its session_id
+  secret:       sessionSecret,       // the session_secret to parse the cookie
+  store:        store,               // we NEED to use a sessionstore. no memorystore please
+  success:      onAuthorizeSuccess,
+  fail:         onAuthorizeFail, 
+}));
+
+function onAuthorizeSuccess(data, accept){
+  console.log('successful connection to socket.io');
+  accept();
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  console.log('socket.io auth err = ', error)
+  console.log('socket.io auth message = ', message)
+  if(error)
+    accept(new Error(message));
+  // this error will be sent to the user as a special error-package
+  // see: http://socket.io/docs/client-api/#socket > error-object
+}
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  if(socket.request.user.gitid) {
+    console.log('socket user = ', socket.request.user)
+    socket.emit('auth', `User ${socket.request.user.name} Authenticated!`)
+  }
+});
+
+server.listen(process.env.PORT, function() {
 	console.log(`Server Started on Port: ${process.env.PORT}!`);
 });
 
@@ -58,7 +101,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requests for files & pages go to express static. Handle the rest seperately on new Routers.
-
 app.use('/', express.static('./dist'));
 
 app.use('/login/', LoginRoutes);
