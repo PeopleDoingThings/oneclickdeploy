8d8059ec4fbb38828bed572b0f4b9cff81736410
Patrick Lynch <patrickflynch@gmail.com>
2016-05-12 11:38:49 -0500

    Changed Login Page Text

--- client/components/login.js
+++ client/components/login.js
@@ -36,24 +36,23 @@ export default class Login extends Component {
               <div className="login col-md-8 col-xs-10 col-centered">
                 <div className="home-title">
                   <h1 className="logo">HyperJs</h1>
-                  <h3 className="tagline">disclaimer: this app is for those who want to deploy their app
-                    but cringe at any complicated deployment process</h3>
+                  <h2 style={{textAlign:'center'}} className="tagline">A fast way to put your node.js apps online</h2>
                 </div>  
-                <h2 className="three-steps">wanna give it a try? 3 simple steps</h2>
+                {/*<h4 className="three-steps">How do I use this?</h4>*/}
                 <div className="node-intro col-md-4 max-size">
-                  <h3>Got node?<Glyphicon glyph="ok" className="glyph" /></h3>
-                  <p>you need a node.js app</p>
+                  <h3>node<Glyphicon glyph="ok" className="glyph" /></h3>
+                  <p>1. Write a node.js app</p>
                   <img src="images/nodejs-logo.png" style={{maxWidth:'50%', height:'auto'}} alt="node.js logo" />
                 </div>
                 <div className="node-intro col-md-4">
-                  <h3>Got github?<Glyphicon glyph="ok" className="glyph" /></h3>
-                  <p>your app is in a public github repo</p>
+                  <h3>github<Glyphicon glyph="ok" className="glyph" /></h3>
+                  <p>2. Put it in a public github repo</p>
                   <img src="images/Octocat.png" style={{maxWidth: '60%', height:'auto'}} alt="Github logo"/>
                 </div>
                 <div className="node-intro col-md-4">
-                  <h3>Got Proc?<Glyphicon glyph="ok" className="glyph" /></h3>
-                  <p>drop in a standard Procfile</p>
-                  <img src="images/Octocat.png" style={{maxWidth: '60%', height:'auto'}} alt="Github logo" />
+                  <h3>Procfile<Glyphicon glyph="ok" className="glyph" /></h3>
+                  <p>3. Add a standard Procfile</p>
+                  <img src="images/gear.png" style={{maxWidth: '55%', height:'auto'}} alt="Procfile logo" />
                 </div>
                 <a href="http://localhost:9001/login/github" className="btn btn-primary">sign in</a>
                 <button type="button" className="btn" onClick={showDetails}> find out more </button>
